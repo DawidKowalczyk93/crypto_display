438699ce1a887bb407c04e77ccbd0687ad455a0d
DawidKowalczyk93 <bandiwaves@gmail.com>
2023-08-21 14:30:10 +0200

    Drag and Drop works propertly

--- src/components/DragnDrop.jsx
+++ src/components/DragnDrop.jsx
@@ -5,54 +5,57 @@ const DragAndDrop = ({data, toDisplay, setToDisplay}) => {
 
     const [list, setList] = useState(data);
     const currentDisplay= useRef();
+    const listLength = useRef();
     const [dragging, setDragging] = useState(false);
     const dragItem = useRef();
     const dragNode = useRef();
 
     const handleDragStart = (e, params) => {
-        console.log('drag start', params)
         dragItem.current = params;
         dragNode.current = e.target;
-        console.log('dragItem.current: ', dragItem.current, 'dragNode.current: ', dragNode.current);
         dragNode.current.addEventListener('dragend', handleDragEnd)
         setDragging(true);
     };
 
     const handleDragEnter = (e , params) => {
-        console.log('dragging enter..')
+
         const currentItem = dragItem.current;
-        console.log('currentItem:  ', currentItem);
         setList((oldList) => {
             let newList = JSON.parse(JSON.stringify(oldList));
             let deleteFromOldList = newList[currentItem.groupIndex].items.splice(currentItem.itemIndex,1)[0];
-            console.log('deleteFromOldList ', deleteFromOldList );
-
             dragItem.current = params;
-            currentDisplay.current = list[0].items[0];
-
+            currentDisplay.current = oldList[0].items[0];
+            console.log(currentDisplay.current);
             if(newList[0].items.length > 0 && params.groupIndex === 0) {
-                console.log('w tablicy diplay jest już item');
                 newList[0].items.splice(0, 1 );
                 newList[0].items.splice(0, 0 , deleteFromOldList);
                 newList[1].items.splice(0,0, currentDisplay.current);
+                currentDisplay.current = deleteFromOldList;
             }else {
                 newList[params.groupIndex].items.splice(params.itemIndex, 0 , deleteFromOldList);
             }
 
-
-
+            listLength.current = newList[0].items.length;
             return newList;
         });
 
     };
     const handleDragEnd = () => {
-        console.log('ending drag...');
-        setToDisplay(currentDisplay.current);
-        console.log('curentDisplay----------:',currentDisplay.current);
+        console.log(currentDisplay.current);
+        console.log(listLength);
+        if(listLength.current === 0) {
+            setToDisplay('')
+        } else {
+            setToDisplay(currentDisplay.current);
+        }
+
+
+
         setDragging(false);
         dragNode.current.removeEventListener('dragend', handleDragEnd);
         dragItem.current = null;
         dragNode.current = null;
+
     };
 
     const getStyles = (params) => {
