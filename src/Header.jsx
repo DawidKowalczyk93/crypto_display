95e4ac3ee5077341d783f8e5bb22647264d6945b
DawidKowalczyk93 <bandiwaves@gmail.com>
2023-08-24 18:12:01 +0200

    add connect wallet button

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Header = () => {
-  return <div className="header__image"></div>
+  return <div className="header__image connect__button">
+    <button className='crypto__block'>Connect Wallet</button>
+  </div>
 };
 
 export default Header
